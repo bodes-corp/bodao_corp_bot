80e4d916f1e23b297ba15de88d5b20f8f1679291
bodao-corp <bodes.corp@gmail.com>
2025-03-16 14:41:31 -0300

    adding poll answer suport 27

--- pkgs/bodaobot/src/rafaelBot/bodaoBotCmd.ts
+++ pkgs/bodaobot/src/rafaelBot/bodaoBotCmd.ts
@@ -70,8 +70,8 @@ export default class BODAO_CMDS {
             if(pollResponse) {
                 console.log('[debug from handleATA] entered poll response if');
                 const params = Requests.MessageToBotTopicRequest(bot,'Foi Inserida uma Nova Ata no Grupo. Por favor leia e responda o quiz')
-                //const response  = await TG_API.sendMessage(bot.botINFO.TOKEN,params);
-                //console.log('[debug from handleATA] response from sendMessage:', JSON.stringify(response));
+                const response  = await TG_API.sendMessage(bot.botINFO.TOKEN,params);
+                console.log('[debug from handleATA] response from sendMessage:', JSON.stringify(response));
                 
                 const newPollData:tgTypes.Poll|undefined = pollResponse.poll
                 console.log('[debug from handleATA] new poll data: ', JSON.stringify(newPollData));
